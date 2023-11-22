1d2983a46d4d4e30bf68cb428dea48b8c4dea629
Benjamin Malengreau <benjamin.malengreau@imi-hydronic.com>
2023-11-22 23:14:13 +0100

    connect to bridge

--- lib/bridge.dart
+++ lib/bridge.dart
@@ -1,4 +1,7 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hue/flutter_hue.dart';
+import 'package:go_router/go_router.dart';
 
 class BridgeScreen extends StatefulWidget {
   final String ip;
@@ -10,26 +13,125 @@ class BridgeScreen extends StatefulWidget {
 }
 
 class _BridgeScreenState extends State<BridgeScreen> {
+  Bridge? _bridge;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Bridge')),
-      body: Container(),
+      body: _bridge == null
+          ? Container()
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
+                  child: Text(
+                    "Bridge",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8, right: 8),
+                  child: Card(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: const Text("Bridge ID"),
+                          subtitle: Text(_bridge!.bridgeId),
+                        ),
+                        ListTile(
+                          title: const Text("IP Address"),
+                          subtitle: Text(_bridge!.ipAddress!),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
     );
   }
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) => _showSnackBar());
+    WidgetsBinding.instance.addPostFrameCallback((_) => _connect(widget.ip));
   }
 
-  void _showSnackBar() {
+  void _connect(String ip) async {
+    _showSnackBar(ip);
+    final savedBridges = await BridgeDiscoveryRepo.fetchSavedBridges();
+    Bridge? bridge = savedBridges.firstWhereOrNull((element) => element.ipAddress == ip);
+
+    if (bridge == null) {
+      await _showPairingBridge();
+      bridge = await BridgeDiscoveryRepo.firstContact(
+        bridgeIpAddr: ip,
+        controller: DiscoveryTimeoutController(),
+      );
+    }
+    if (bridge == null) {
+      _hideSnackBar();
+      await _showPairingFailed();
+      return _popBack();
+    }
+    setState(() {
+      _bridge = bridge;
+    });
+
+    _hideSnackBar();
+  }
+
+  void _showSnackBar(ip) {
     final snackBar = SnackBar(
-      content: Text('Connecting to bridge (IP: ${widget.ip})'),
+      content: Text('Connecting to bridge (IP: $ip)'),
       dismissDirection: DismissDirection.none,
       duration: const Duration(days: 1),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
+
+  void _hideSnackBar() {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+  }
+
+  void _popBack() {
+    GoRouter.of(context).pop();
+  }
+
+  _showPairingFailed() => showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Pairing Failed'),
+            content: const Text('Unable to pair with bridge.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+
+  _showPairingBridge() => showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Pairing Bridge'),
+            content: const Text(
+              "The bridge is not paired with this app. "
+              "After pressing 'Start Pairing', press the central button on the bridge.",
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Start Pairing'),
+              ),
+            ],
+          );
+        },
+      );
 }
